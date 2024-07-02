460493059df19e774d139e6f7b6ce99dac9a3071
ohmin839 <ohmin839.echoda.resident@gmail.com>
2024-07-03 00:54:51 +0900

    From unittest to pytest

--- pyplgrapi/tests/test_to_words_list.py
+++ pyplgrapi/tests/test_to_words_list.py
@@ -1,17 +1,18 @@
-import unittest
-from parameterized import parameterized
+import pytest
 from pyplgrapi import to_words_list
 
-class ToWordsList(unittest.TestCase):
-    @parameterized.expand([
-        ["", []],
-        ["α", ["α"]],
-        ["δ'", ["δ'"]],
-        ["ὁ ἄνθρώπός τις", ["ὁ", "ἄνθρώπός", "τις"]],
-    ])
-    def test_to_words_list(self, text, expected):
-        actual = to_words_list(text)
-        self.assertEqual(expected, actual)
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize(
+    [
+        "text",
+        "expected"
+    ],
+    [
+        pytest.param("", []),
+        pytest.param("α", ["α"]),
+        pytest.param("δ'", ["δ'"]),
+        pytest.param("ὁ ἄνθρώπός τις", ["ὁ", "ἄνθρώπός", "τις"]),
+    ]
+)
+def test_to_words_list(text, expected):
+    actual = to_words_list(text)
+    assert expected == actual

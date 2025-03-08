7d253c983516a6c33fadb8b08129fe685bd16b13
ohmin839 <ohmin839.echoda.resident@gmail.com>
2025-03-08 10:12:42 +0900

    Modified collector CLI with ordered-set

--- pyplgrcore/src/pyplgrcore/cli/collector.py
+++ pyplgrcore/src/pyplgrcore/cli/collector.py
@@ -1,10 +1,16 @@
 import sys
+from ordered_set import OrderedSet
+
 from pyplgrcore.api import to_words_list
 
 def main():
+    words_set = OrderedSet() 
     for line in sys.stdin:
-        words = to_words_list(line)
-        for w in words:
+        words_list = to_words_list(line)
+        for w in words_list:
+            words_set.add(w)
+    else:
+        for w in words_set:
             print(w)
 
 if __name__ == '__main__':
